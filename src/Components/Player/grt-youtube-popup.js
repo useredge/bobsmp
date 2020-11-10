7fe551a81a07718d42c604fad97973bd8270b68a
useredge <evilpokefan@gmail.com>
2020-11-10 21:41:43 +0100

    list phase 2: bigger player, button fixed, 3x3 blog layout

--- src/Components/Player/grt-youtube-popup.js
+++ src/Components/Player/grt-youtube-popup.js
@@ -1,4 +1,5 @@
 import jQuery from 'jquery';
+import './grt-youtube-popup.css'
 
 /*!
  * GRT Youtube Popup - jQuery Plugin
@@ -34,7 +35,7 @@ import jQuery from 'jquery';
 				$(this).on( "click", function() {
 					 $("body").append('<div class="grtyoutube-popup '+settings.theme+'">'+
 								'<div class="grtyoutube-popup-content">'+
-									'<span class="grtyoutube-popup-close"><span class="space">Back to site</span></span>'+
+									'<span class="grtyoutube-popup-close"><i class="fas fa-window-close"></i><span class="space">BACK TO SITE</span></span>'+
 									'<iframe class="grtyoutube-iframe" src="https://www.youtube.com/embed/'+settings.videoID+'?rel=0&wmode=transparent&autoplay='+settings.autoPlay+'&iv_load_policy=3" allowfullscreen frameborder="0" allow="autoplay; fullscreen"></iframe>'+
 								'</div>'+
 							'</div>');
