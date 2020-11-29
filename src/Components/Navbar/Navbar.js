03ad19da87f8ab68fdf5f0eafb9c16b269822897
useredge <evilpokefan@gmail.com>
2020-11-29 02:16:37 +0100

    new navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,26 +1,38 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from 'react-router-dom'
 import { AnimateOnChange } from 'react-animation'
 import './Navbar.css';
 
 const Navbar = () => {
 
-    const [visible, setVisible] = useState(false)
+    const [visible, setVisible] = useState(false);
 
-    let blue = "blue";
-    let purple = "purple";
-    let green = "green";
+    const [color, setColor] = useState('#25c5e7')
 
-    // eslint-disable-next-line default-case
+    let defColor = '';
+    let location = window.location.pathname
 
-    switch (window.location.pathname.includes('/blog')) {
-        case true: 
-        purple += " permapurple"
+    switch(location) {
+        default: 
+            defColor = '#25c5e7'
         break;
-        case false:
+        case '/' :
+            defColor = '#25c5e7'
         break;
-    } 
-    
+        case '/blog' :
+            defColor = '#e18fff'
+        break;
+        case '/help' :
+            defColor = '#9cff8f'
+        break;
+        case '/shop' :
+            defColor = '#ff4545'
+        break;
+    }
+
+    useEffect(() => {
+        setColor(defColor)
+    }, [])
 
     return (
         <div>
@@ -32,20 +44,32 @@ const Navbar = () => {
                     <i className={visible? "fas fa-times" : "fas fa-bars"} onClick={() => {setVisible(!visible)}}/>
 
             </AnimateOnChange>
-        <div className={visible ? "navContainer visible" : "navContainer hidden"}>
+        <div className={visible ? "navContainer visible" : "navContainer hidden"} style={{backgroundColor: color}}>
             <nav>
                 <ul className="nav-links">
                     <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
-                        <li className="home">HOME <i class="fas fa-home"/></li>
+                        <li 
+                        className={(location === '/') ? "home active" : "home"}
+                        onMouseEnter={() => {setColor('#25c5e7')}}
+                        onMouseLeave={() => {setColor(defColor)}}><img src="/images/Pixel Icons/home.png" className="navIcon"/><p className="navTitle">HOME</p></li>
                     </Link>
                     <Link to='/blog' style={{ textDecoration: 'none', color: 'white' }}>
-                        <li className="news">NEWS <i class="fas fa-newspaper"/></li>
+                        <li 
+                        className={(location === '/blog') ? "news active" : "news"} 
+                        onMouseEnter={() => {setColor('#e18fff')}}
+                        onMouseLeave={() => {setColor(defColor)}}><img src="/images/Pixel Icons/chat.png" className="navIcon"/><p className="navTitle">NEWS</p></li>
                     </Link>
                     <Link to='/help' style={{ textDecoration: 'none', color: 'white' }}>
-                        <li className="help">HELP <i class="fas fa-question"/></li>
+                        <li 
+                        className={(location === '/help') ? "help active" : "help"} 
+                        onMouseEnter={() => {setColor('#9cff8f')}}
+                        onMouseLeave={() => {setColor(defColor)}}><img src="/images/Pixel Icons/heart.png" className="navIcon"/><p className="navTitle">HELP</p></li>
                     </Link>
                     <a href="https://shop.bobssmp.com/" style={{ textDecoration: 'none', color: 'white' }}>
-                        <li className="shop">SHOP <i class="fas fa-shopping-cart"/></li>
+                        <li 
+                        className="shop"
+                        onMouseEnter={() => {setColor('#ff4545')}}
+                        onMouseLeave={() => {setColor(defColor)}}><img src="/images/Pixel Icons/basket.png" className="navIcon"/><p className="navTitle">SHOP</p></li>
                     </a>
                 </ul>
             </nav>
