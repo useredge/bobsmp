573a9ff9adc2c6cbcd98d6cefb1793de52883f0d
useredge <evilpokefan@gmail.com>
2020-12-20 03:45:00 +0100

    swiper finalized

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -1,27 +1,39 @@
 import {useEffect, useState} from "react";
-import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
 import './Blog.css';
 import client from '../client'
 import BlogItem from '../BlogItem/BlogItem'
 import Footer from "../Footer/Footer"
 import { motion } from 'framer-motion'
+import { AnimateOnChange } from 'react-animation'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import SwiperCore from 'swiper'
-import 'swiper/swiper-bundle.css'
+import SwiperCore, {Navigation, EffectCoverflow} from 'swiper'
+import 'swiper/swiper.scss'
+import 'swiper/components/navigation/navigation.scss'
+import 'swiper/components/effect-coverflow/effect-coverflow.scss'
 import {Link} from 'react-router-dom'
 
 const Blog = () => {
+
+  SwiperCore.use([EffectCoverflow])
   
   const [articles, setArticles] = useState([]);
+  const [current, setCurrent] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
   
   useEffect(() => {
     fetchData();
   }, []);
   
   const fetchData = async() => {
-      const resp = await client.getEntries();
+    setIsLoading(true);
+      const resp = await client.getEntries({
+        content_type: 'blog',
+        order: '-fields.date'
+      });
       setArticles(resp.items);
+      setCurrent(resp.items[1])
+      setIsLoading(false)
   }
   
   const list = {
@@ -44,10 +56,26 @@ const Blog = () => {
     },
   }
 
-  const slides = articles.slice(0, 3).map((article, index) => <SwiperSlide tag={Link} to={"blog/" + article.fields.path} key={index}><img src={article.fields.thumbnail.fields.file.url} className="slide"/></SwiperSlide>)
+  const slides = articles.slice(0, 3).map((article, index) => 
+  <SwiperSlide
+      tag={Link}
+      to={"blog/" + article.fields.path} 
+      key={index} 
+      style={{backgroundImage: 'url(' + article.fields.thumbnail.fields.file.url + ')'}}
+      alt={article.fields.date}
+      virtualIndex={index}
+      >
+  </SwiperSlide>)
 
-  if (articles.length === 0) return <h1 style={{fontSize: '100px', color: 'transparent', width: '100%', textAlign: 'center', fontFamily: 'MCTen'}}>Loading...</h1>
+  const swapPositions = (array, a ,b) => {
+    [array[a], array[b]] = [array[b], array[a]]
+  }
 
+  swapPositions(slides, 2, 0);
+  swapPositions(slides, 1, 2);
+
+  if (isLoading) return 'Loading...'
+  
   return (
       <motion.div
         variants={list}
@@ -56,17 +84,43 @@ const Blog = () => {
         exit="hidden"
         style={{backgroundColor: '#190f1d'}}
       >
+
       <Navbar />
-        <div className="centerer">
-          <div className="carousel">
+
+      <div className="blogHeader">
+
+        <p className="updates">Keep up with our latest updates!</p>
+          <AnimateOnChange
+            animationIn="fadeIn"
+            animationOut="fadeOut"
+            durationOut={1000}>
+
+            <p className="selectedArticle">{current.fields.title}</p>
+
+            <div className="statusDate">
+              <p>{current.fields.status}&nbsp;-</p>
+              <p>&nbsp;{current.fields.date}</p>
+            </div>
+            </AnimateOnChange>
+
+      </div>
+
           <Swiper 
             id="main"
-            spaceBetweenSlides={0}
+            effect="coverflow"
+            loop={false}
+            grabCursor={true}
+            coverflowEffect={{rotate: 20, depth: 1000, slideShadows: true}}
+            centeredSlides={true}
+            spaceBetween={-200}
+            slidesPerView={3}
+            initialSlide={1}
+            preloadImages={true}
+            onSlideChange={(swiper) => {setCurrent(articles[swiper.realIndex])}}
             >
             {slides}
           </Swiper>
-          </div>
-        </div>
+
         <Footer/>
     </motion.div>
     )
